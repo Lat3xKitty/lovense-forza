8c6b7d602eadff188ad613adfc0906f76f26c0f5
TrappyChastity <107673703+TrappyChastity@users.noreply.github.com>
2022-06-17 14:57:40 +0200

    feat: cleanup code, Vastly improve docas, add start command to package.json

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# lovense-forza
-Forza Telemetry to Control Lovense Toys - Pick between RPM, Speed, Surface Rumble, Power, Torque &amp; Accel/Brake force
+# lovense-forza #
+
+Forza Telemetry to Control Lovense Toys
+
+## Running and Stopping ##
+
+`npm start`
+
+Triggers that start function in `package.json` which simply runs node on `index.js`
+
+`npm stop`
+
+## Telemetry to choose from ##
+
+Telemetry to pick between:
+
+* `rpm`            _- RPM from the Engine **(Best all around experience)**_
+* `speed`          _- Current Speed, slow but fun_
+* `rumble-average` _- Surface Rumble **(Average)**_
+* `power`          _- Not quite sure, seems to be a similar thing to RPM._
+* `torque`         _- Torque of the Vehicle_
+* `accel`          _- How hard are you pushing down on the **Accelerate** Pedal_
+* `brake`          _- How hard are you pushing down on the **Brake** Pedal_
+
+> Enter the required setting on `index.js` line **22** `var vibrationFrom = 'rpm';`
+
+## Where to get toy Data ##
+
+Enable Lovense and connect it to a device `pc, android, ios` then visit the API pagethat is now available on your lan that will return a `json` document by clicking [api/lan/getToys](https://api.lovense.com/api/lan/getToys)
+
+You are looking for the
+
+* **platform** - _pc, android, ios_
+* **domain** - _The domain Lovense is on_ e.g `192-168-178-16.lovense.club`
+* **port** - _Will proably be `30010`_
+* **toyID** - _this can be found in the `toys` section, `"status": 1` means the currently active toy (_if one is connected_) if you have two or more (_including generations_) of the same device, the are likely to show up with the same name (_e.g Domi and Domi 2 both listed as domi_) but have different `hVersions`. If you've given them nicknames that's even easier_
--- index.js
+++ index.js
@@ -1,171 +1,127 @@
 const Forza = require('forza.js');
+const nodeFetch = import('node-fetch');
 const forza = new Forza.default();
 
-const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const fetch = (...args) => nodeFetch.then(({ default: fetch }) => fetch(...args));
 
 // Go to https://api.lovense.com/api/lan/getToys
 // And Grab a few fields
-var toy = 'TOY_ID';           // Find the ToyId (random sort of Characters to Vibrate) 
-var platform = "android";     // What Device you are using. PC / Android / iOS
-var domain = "192-168-1-98.lovense.club"; // Domain from Lovense
-var port = "30010";           // httpsPort 
+const toyID = 'F96B83E9AFF8'; // Find the ToyId (random sort of Characters to Vibrate)
+const platform = 'pc'; // What Device you are using. PC / Android / iOS
+const domain = '192-168-178-16.lovense.club'; // Domain from Lovense
+const port = '30010'; // httpsPort
 
 // Select what it will react to
-// rpm            - RPM from the Engine        (Best all around experience)
+// rpm            - RPM from the Engine (Best all around experience)
 // rumble-average - Surface Rumble (Average)
 // speed          - Speed, slow but fun
 // power          - Not quite sure, seems to be a similar thing to rpm.
 // torque         - Torque of the Vehicle
 // brake          - How hard are you pushing down on the Brake Pedal
 // accel          - How hard are you pushing down on the Accel Pedal
-var vibrationFrom = 'speed';
-var maxVibration = 20;      // Max Power of 1-20 (Use 20 if ya wanna go wild)
+const vibrationFrom = 'rpm';
+const maxVibration = 15; // Max Power of 1-20 (Use 20 if ya wanna go wild)
 
-(async function () {
-    await forza.loadGames();
-    forza.startAllGameSockets();
+function getProp(myData) {
+  const out = [];
+  for (const key in myData)
+    if (myData.hasOwnProperty(key)) out.push(key + '=' + encodeURIComponent(myData[key]));
+  return out.join('&');
+}
 
-    var processData = function(data) {
-      // Do something with data
+function lovenseUrl(endpoint, values) {
+  if (endpoint.indexOf('A') === 0 && platform == 'pc') endpoint = endpoint.substring(1); // Removes the A for PC specific.
 
-      /**
-       * 0-1 = 0% - 100%
-       */
-      var vibration = 0;
-      if (data.isRaceOn === 0) {
-      }
-      else {
-        switch (vibrationFrom) {
-          case 'rpm':
-            vibration = (data.currentEngineRpm - data.engineIdleRpm)
-                        /
-                        (data.engineMaxRpm - data.engineIdleRpm);
-            break;
+  console.log(
+    `https://${domain}:${port}/${endpoint}` +
+      (Object.keys(values).length > 0 ? `?${getProp(values)}` : '')
+  );
 
-          case 'rumble-average':
-            vibration = (
-              ((
-                data.surfaceRumbleFrontLeft + data.surfaceRumbleFrontRight +
-                data.surfaceRumbleRearLeft + data.surfaceRumbleRearRight
-              )
-              / 4)
-              * 1.666
-              // Seems to max at 6, so we're scaling it up to 10.
-            );
-            break;
-
-            // One wheel is always max?
-          // case 'rumble-max':
-          //   vibration = (
-          //     Math.max(
-          //       data.surfaceRumbleFrontLeft, data.surfaceRumbleFrontRight,
-          //       data.surfaceRumbleRearLeft, data.surfaceRumbleRearRight
-          //     )
-          //     * 1.666
-          //   );
-          //   break;
-
-          case 'speed':
-            vibration = data.speed / 100;
-            break;
-
-          case 'power':
-            vibration = Math.abs(data.power) / 1000000;
-            break;
-
-          case 'torque':
-            vibration = Math.abs(data.torque) / 1000;
-            break;
-
-          case 'brake':
-            vibration = data.brake / 255;
-            break;
-
-          case 'accel':
-            vibration = data.accel / 255;
-            break;
-        }
-
-        // console.log(vibrationFrom, vibration.toFixed(2));
-
-        var vibrationInt = Math.round(vibration * maxVibration);
-        if (vibrationInt > maxVibration) {
-          vibrationInt = maxVibration;
-        }
-      }
-
-      sendVibration(vibrationInt);
-    }
-
-    forza.on('telemetry', throttle(
-      processData,
-      100
-    ));
-
-
-})()
+  return (
+    `https://${domain}:${port}/${endpoint}` +
+    (Object.keys(values).length > 0 ? `?${getProp(values)}` : '')
+  );
+}
 
 // Throttling Function
 const throttle = (func, limit) => {
   let inThrottle;
-  return function() {
+  return function () {
     const args = arguments;
     const context = this;
     if (!inThrottle) {
       func.apply(context, args);
       inThrottle = true;
-      setTimeout(() => inThrottle = false, limit);
+      setTimeout(() => (inThrottle = false), limit);
     }
-  }
-}
+  };
+};
 
-var lastVibration = 0;
-const sendVibration = function(vibration) {
+let lastVibration = 0;
+const sendVibration = function (vibration) {
   if (vibration !== lastVibration) {
     lastVibration = vibration;
-
-    var power = vibration;
-    if (!power) { power = 0; }
-
-    var params = {
-      sec: 0,
-      v: power
-    }
-    if (toy !== 'ALL') {
-      params.t = toy;
-    }
-    console.log("Sending Vibration", vibration);
-    fetch(lovenseUrl("AVibrate", params))
-    .then(async res =>
-      console.log("Successfully sent vibration", vibration)
+    const power = vibration || 0;
+    const params = { sec: 0, v: power };
+    if (toyID !== 'ALL') params.t = toyID;
+    console.log('Sending Vibration', vibration);
+    fetch(lovenseUrl('AVibrate', params)).then(async (res) =>
+      console.log('Successfully sent vibration', vibration)
     );
   }
-}
+};
 
-function lovenseUrl(endpoint, values) {
-  if (endpoint.indexOf("A") === 0 && platform == "pc") {
-    endpoint = endpoint.substring(1); // Removes the A for PC specific.
-  }
+(async function () {
+  await forza.loadGames();
+  forza.startAllGameSockets();
 
-  console.log(
-    `https://${domain}:${port}/${endpoint}` +
-    (Object.keys(values).length > 0 ? `?${getProp(values)}` : "")
-  );
+  const processData = function (data) {
+    // Do something with data
 
-
-  return (
-    `https://${domain}:${port}/${endpoint}` +
-    (Object.keys(values).length > 0 ? `?${getProp(values)}` : "")
-  );
-
-  function getProp(myData) {
-    var out = [];
-    for (var key in myData) {
-      if (myData.hasOwnProperty(key)) {
-        out.push(key + "=" + encodeURIComponent(myData[key]));
+    /**
+     * Vibration Percentage expressed as number between `0-1`
+     * @type {number}
+     * @example
+     * var vibration = 0.5; // 50%
+     */
+    let vibration = 0.1;
+    if (data.isRaceOn !== 0) {
+      switch (vibrationFrom) {
+        case 'rpm':
+          vibration =
+            (data.currentEngineRpm - data.engineIdleRpm) / (data.engineMaxRpm - data.engineIdleRpm);
+          break;
+        case 'rumble-average':
+          vibration =
+            ((data.surfaceRumbleFrontLeft +
+              data.surfaceRumbleFrontRight +
+              data.surfaceRumbleRearLeft +
+              data.surfaceRumbleRearRight) /
+              4) *
+            1.666;
+          // Seems to max at 6, so we're scaling it up to 10.
+          break;
+        case 'speed':
+          vibration = data.speed / 100;
+          break;
+        case 'power':
+          vibration = Math.abs(data.power) / 1000000;
+          break;
+        case 'torque':
+          vibration = Math.abs(data.torque) / 1000;
+          break;
+        case 'brake':
+          vibration = data.brake / 255;
+          break;
+        case 'accel':
+          vibration = data.accel / 255;
+          break;
       }
+
+      let vibrationInt = Math.round(vibration * maxVibration);
+      if (vibrationInt > maxVibration) vibrationInt = maxVibration;
+      sendVibration(vibrationInt);
+      forza.on('telemetry', throttle(processData, 100));
     }
-    return out.join("&");
-  }
-}
+  };
+})();
